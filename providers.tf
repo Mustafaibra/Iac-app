9c8efa789c2e3c5515de2cf3ff87f29f9b4de104
mustafaibrahim <swe.mustafaibrahim@gmail.com>
2025-05-11 03:19:58 +0300

    using workspace

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket         = "mustafa-tf-state2025"
-    key            = "env/${TER_ENV}/terraform.tfstate"
+    key            = "env/state/terraform.tfstate"
     region         = "eu-north-1" # Replace with your desired region
     dynamodb_table = "terraform-locks"
 
